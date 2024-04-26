a7d4f81d42c5a229ec634a74e19e8957c5d3d443
ZacAttack <dexrn@hotmail.com>
2024-04-25 18:55:42 -0700

    initial commit, no idea if this project will get far but I certainly hope it does.

---
+++ chunkParsing.ts
@@ -0,0 +1,83 @@
+/*
+Copyright 2024 Dexrn ZacAttack
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+import { readdir, readFile, writeFile, stat } from "fs/promises";
+import { join, resolve, relative } from "path";
+
+const path: string = resolve(process.argv[2]!);
+
+const lEndian = false; 
+
+// never used this before oooh
+interface indice {
+    offset: number;
+    length: number;
+}
+
+interface timestamp {
+    timestamp: number;
+}
+
+let timestamps: timestamp[] = [];
+let indices: indice[] = [];
+
+interface region {
+    indices: indice[];
+    timestamps: timestamp[];
+}
+
+let regionData: region[] = [];
+
+function getUint24(byteOffset: number, dataView: DataView, lEndian: boolean = true): number {
+    const byte1 = dataView.getUint8(byteOffset);
+    const byte2 = dataView.getUint8(byteOffset + 1);
+    const byte3 = dataView.getUint8(byteOffset + 2);
+
+    let result = lEndian ? (byte3 << 16) | (byte2 << 8) | byte1 : (byte1 << 16) | (byte2 << 8) | byte3;
+
+    return result;
+}
+
+async function read() {
+    parseRegion(await readFile(path));
+}
+
+async function parseRegion(file: Buffer) {
+    const fileDV = new DataView(file.buffer);
+    let curOffset = 0;
+    
+    while (curOffset < 4095) {
+        var cIOffset = getUint24(curOffset, fileDV, lEndian) * 4096;
+        curOffset += 3;
+        var cILength = fileDV.getUint8(curOffset) * 4096;
+        curOffset += 1;
+        indices.push({offset: cIOffset, length: cILength});
+    }
+    while (curOffset < 8191) {
+        var iTimestamp = fileDV.getUint32(curOffset, lEndian);
+        curOffset += 4;
+        timestamps.push({timestamp: iTimestamp});
+    }
+    regionData.push({indices: indices, timestamps: timestamps});
+}
+
+read();
